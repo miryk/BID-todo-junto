d67b097e93a85709544c246e53abecf241d5e4b4
Miry Konishi <miry.konishi@gmail.com>
2023-01-12 20:28:37 -0300

    Update new Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import PersonCard from './components/personCard';
 
 function App() {
   return (
     <div className="App">
-
+      <PersonCard info={{firstName: "Jane", lastName: "Doe", age: 45, hairColor: "Black"}}/>
     </div>
   );
 }
---
+++ src/components/personCard.jsx
@@ -0,0 +1,33 @@
+import { Component } from "react";
+
+
+class PersonCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: props.info.age
+    }
+  }
+  addYear = () => {
+    this.setState({
+      age: this.state.age+1
+    });
+  }
+
+  
+  render() { 
+    const { firstName, lastName, hairColor} = this.props.info;
+    return (
+      <>
+        <h1>{lastName}, {firstName}</h1>
+        <p>Age: {this.state.age}</p>
+        <p>Hair Color: {hairColor}</p>
+        <button onClick={this.addYear}>{firstName} {lastName} Bithday Button</button>
+      </>
+    );
+  }
+}
+
+
+
+export default PersonCard;
