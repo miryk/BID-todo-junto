e72f0fb438097f0381c141230bb240a2c74c4801
Miry Konishi <miry.konishi@gmail.com>
2023-01-12 23:03:33 -0300

    Update CSS styles

--- src/components/personCard.jsx
+++ src/components/personCard.jsx
@@ -1,5 +1,13 @@
 import { Component } from "react";
 
+const btnStyle = {
+  padding: "10px", 
+  borderRadius: "5px",
+  backgroundColor: "ligthgray",
+  margin: "10px -15px",
+  border: "none",
+  color: "cadetblue"
+}
 
 class PersonCard extends Component {
   constructor(props) {
@@ -22,7 +30,7 @@ class PersonCard extends Component {
         <h1>{lastName}, {firstName}</h1>
         <p>Age: {this.state.age}</p>
         <p>Hair Color: {hairColor}</p>
-        <button onClick={this.addYear}>{firstName} {lastName} Bithday Button</button>
+        <button style={btnStyle} onClick={this.addYear}>Bithday Button for {firstName} {lastName} </button>
       </div>
     );
   }
